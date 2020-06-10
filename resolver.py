bc9c64a1c10e1ee501435c66abb1675885fa33b5
Ramon Garcia <47482671+ramonf-garcia@users.noreply.github.com>
2020-06-10 13:38:10 -0700

    Adding validation on empty results

--- resolver.py
+++ resolver.py
@@ -4,7 +4,7 @@ import threading
 
 NAMESERVERS=['8.8.8.8','8.8.4.4','9.9.9.9']
 
-def resolve_domain(domain, log, error_log, record='A', retry=10, timeout=10):
+def resolve_domain(domain, app_log, error_log, record='A', retry=10, timeout=10):
     ''' Query using resolver class and return a dictionary with its results '''
     results = {}
     try:
@@ -15,16 +15,19 @@ def resolve_domain(domain, log, error_log, record='A', retry=10, timeout=10):
             qname=record,
             verbose=True
         )
-        domains = [domain]
-        results = resolver.resolve(domains)
+        results = resolver.resolve([domain])
     except Exception as error:
         error_log.error(f'Failed to query domain: {domain} with error: {error}')
-    log.info(results['success'])
-    log.error(results['error'])
+    finally:
+        if results:
+            app_log.info(results['success'])
+            error_log.error(results['error'])
+        else:
+            error_log('Failed to get a response for the resolver')
 
-def setup_log(name, log_file, level=logging.DEBUG):
+def setup_log(name, filename, level=logging.DEBUG):
     ''' Create a log file for the application '''
-    handler = logging.FileHandler(log_file)
+    handler = logging.FileHandler(filename)
     handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
     logger = logging.getLogger(name)
     logger.setLevel(level)
@@ -32,7 +35,7 @@ def setup_log(name, log_file, level=logging.DEBUG):
     return logger
 
 if __name__ == "__main__":
-    error_log = setup_log('error', 'resolver.err')
+    error_log = setup_log('error_log', 'resolver.err')
     resolver_log = setup_log('resolver_log', 'resolver.log')
 
     domain = input('Which domain do I have to resolve? ')
